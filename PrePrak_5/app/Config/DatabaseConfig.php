e4ab146f023110a2f401e7e8c617fe059af8102e
IAshura <127363417+IAshura@users.noreply.github.com>
2023-12-08 13:43:42 +0700

    Add files via upload

---
+++ PrePrak_5/app/Config/DatabaseConfig.php
@@ -0,0 +1,11 @@
+<?Php
+
+namespace app\Config;
+
+class DatabaseConfig{
+    public $host = "localhost";
+    public $user = "root";
+    public $password = "";
+    public $database_name = "web_modul5_latihan";
+    public $port = 3306;
+}
